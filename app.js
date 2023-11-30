c2ad3c80be2e6f4a7b45e4c1a181bc612b61731f
Melissa Ozcan <ozcanmelissa@gmail.com>
2023-11-30 13:30:49 -0500

    Folder structure

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log('Your routes will be running on http://localhost:3000');
+});
